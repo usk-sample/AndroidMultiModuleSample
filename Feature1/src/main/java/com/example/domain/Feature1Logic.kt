1512fe16f65d4e96ac127d79783d69677a5e3c6c
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2022-01-20 16:32:14 +0900

    feat: add Flow

--- Feature1/src/main/java/com/example/domain/Feature1Logic.kt
+++ Feature1/src/main/java/com/example/domain/Feature1Logic.kt
@@ -1,15 +1,28 @@
 package com.example.domain
 
+import android.util.Log
 import com.example.infrastructure.api.Api
 import com.example.infrastructure.store.DataStore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import java.lang.Exception
 
 class Feature1Logic(
     private val api: Api = Api(),
     private val store: DataStore = DataStore()
 ) {
 
-    fun listRepos() {
-
+    fun listRepos(): Flow<List<RepoModel>> {
+        return flow {
+            try {
+                emit(api.service.listRepos("usk2000").execute().body()!!.map { RepoTranslator.convert(it) })
+            } catch (e: Exception) {
+                Log.e("Feature1Logic", e.toString())
+                emit(listOf())
+            }
+        }.flowOn(Dispatchers.IO)
     }
 
 }
