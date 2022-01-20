adcd4af256c741e37071a35713b0c22ab4079f78
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2022-01-21 08:52:50 +0900

    feat: ViewModel

--- app/src/main/java/com/example/androidmultimodulesample/MainViewModel.kt
+++ app/src/main/java/com/example/androidmultimodulesample/MainViewModel.kt
@@ -1,17 +1,25 @@
 package com.example.androidmultimodulesample
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.*
 import com.example.domain.Feature1Logic
 import com.example.domain.RepoModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val feature1Logic: Feature1Logic = Feature1Logic()
 ): ViewModel() {
 
-    val repos: LiveData<List<RepoModel>> = feature1Logic.listRepos().asLiveData()
+    private val _repos: MutableLiveData<List<RepoModel>> = MutableLiveData()
+    val repos: LiveData<List<RepoModel>> = _repos
 
-    //TODO: userを指定して実行するためにはどのようにするか
-
+    fun listRepo(user: String) {
+        viewModelScope.launch {
+            feature1Logic.listRepos()
+                .collect {
+                    _repos.postValue(it)
+                }
+        }
+    }
+    
 }
