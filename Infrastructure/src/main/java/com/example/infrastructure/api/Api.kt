e3db0ddee53fc950ab1800336aa655da227c014f
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2022-01-21 09:38:48 +0900

    feat: add converter factory

--- Infrastructure/src/main/java/com/example/infrastructure/api/Api.kt
+++ Infrastructure/src/main/java/com/example/infrastructure/api/Api.kt
@@ -1,7 +1,9 @@
 package com.example.infrastructure.api
 
-import retrofit2.Call
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -12,8 +14,13 @@ interface GithubService {
 
 class Api {
 
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("https://api.github.com/")
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
     val service: GithubService = retrofit.create(GithubService::class.java)
 
